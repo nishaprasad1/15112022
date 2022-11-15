803c63a9d1f645fe88e0f7fcc8121b82fb819ee8
nishaprasad1 <114492585+nishaprasad1@users.noreply.github.com>
2022-11-15 11:37:08 +0530

    Add files via upload

---
+++ DotNet/AsyncParallelTest/DemoApp/Program2.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    private static Task DoComputation(int limit)
+    {
+        Console.Write("Computing...");
+        Console.WriteLine("In Computaion Function Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+        var c = new Computation();
+        var w = new System.Diagnostics.Stopwatch();
+        w.Start();
+        return c.ComputeAsync(1, limit)
+                .ContinueWith(previous => 
+                {   
+                    long r = previous.Result;
+                    w.Stop();
+                    Console.WriteLine("After Compute Async completes Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+                    Console.WriteLine("Done!");
+                    Console.WriteLine("Result = {0}, computed in {1:0.000} seconds.", r, w.Elapsed.TotalSeconds);
+                });
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(args[0]);
+        Console.WriteLine("In Main Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+        var job = DoComputation(n);
+        while(!job.IsCompleted)
+        {
+            Console.Write(". {0}",Thread.CurrentThread.ManagedThreadId );
+            Thread.Sleep(500);
+        }
+    }
+
+    class Computation
+    {
+        public long Compute(int first, int last)
+        {
+            Console.WriteLine("In Run in Compute from ComputeAsync Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+            return Enumerable.Range(first, last - first + 1)
+                        .Select(Worker.DoWork)
+                        .Sum();
+        }
+
+        public Task<long> ComputeAsync(int first, int last)
+        {
+            Console.WriteLine("In Compute Async Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+            //the operation passed to Run is invoked by a pooled
+            //thread allowing the caller thread to resume and obtain
+            //the result of invocation from the retuned Task once the
+            //pooled thread completes that invocation
+            return Task<long>.Run(() => Compute(first, last));
+        }
+    }
+}
---
+++ DotNet/AsyncParallelTest/DemoApp/Program3.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    //a method defined with async modifier returns a Task
+    //through await statement which yields the task returned
+    //by its target expression combined with another task
+    //which executes code after that await statement
+    private static async Task DoComputation(int limit)
+    {
+        Console.Write("Computing...");
+        Console.WriteLine("In Computation Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+        var c = new Computation();
+        var w = new System.Diagnostics.Stopwatch();
+        w.Start();
+        long r = await c.ComputeAsync(1, limit);
+        Console.WriteLine("In Computation After Async Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+        w.Stop();
+        Console.WriteLine("Done!");
+        Console.WriteLine("Result = {0}, computed in {1:0.000} seconds.", r, w.Elapsed.TotalSeconds);
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(args[0]);
+        var job = DoComputation(n);
+        while(!job.IsCompleted)
+        {
+            Console.Write(". {0}",Thread.CurrentThread.ManagedThreadId );
+            Thread.Sleep(500);
+        }
+    }
+
+    class Computation
+    {
+        public long Compute(int first, int last)
+        {
+             Console.WriteLine("In Comp in run Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+            return Enumerable.Range(first, last - first + 1)
+                        .AsParallel()
+                        .Select(Worker.DoWork)
+                        .Sum();
+        }
+
+        public Task<long> ComputeAsync(int first, int last)
+        {
+            //the operation passed to Run is invoked by a pooled
+            //thread allowing the caller thread to resume and obtain
+            //the result of invocation from the retuned Task once the
+            //pooled thread completes that invocation
+            Console.WriteLine("In Compute Async Task Done by : {0} Thread", Thread.CurrentThread.ManagedThreadId);
+            return Task<long>.Run(() => Compute(first, last));
+        }
+    }
+}
